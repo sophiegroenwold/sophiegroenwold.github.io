de99a67419f93741ebcda577742993d6e20a5ebf
sophiegroenwold <sophiegroenwold@umail.ucsb.edu>
2019-08-19 13:20:18 -0700

    added resume and details

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-function rg(m, n) {
-    return Math.floor( Math.random() * (n - m + 1) ) + m;
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
-
-function hex(i) {
-    return i.toString(16);
-}
-
-function randColor() {
-    return '#' + hex(rg(1, 15)) + hex(rg(1, 15)) + hex(rg(1, 15)) +
-        hex(rg(1, 15)) + hex(rg(1, 15)) + hex(rg(1, 15));
-}
-
-$("h1").mouseover(function() {
-    $(this).css("color",randColor());
-});
+var div = document.getElementById( 'colorchange' );
+div.onmouseover = function() {
+  this.style.color = getRandomColor();
+  var h2s = this.getElementsByTagName( 'div' );
+};
+div.onmouseout = function() {
+  this.style.color = 'black';
+};
